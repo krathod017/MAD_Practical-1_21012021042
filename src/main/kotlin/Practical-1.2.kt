a3d1b6558d4a286fa6a5f9ebb06f4d632488b4fd
krutikarathod17 <krutikarathod21@gnu.ac.in>
2023-07-18 22:33:49 +0530

    Practical-1.2

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var a = 5
+    println("Integer value: $a")
+    var d=a.toDouble()
+    println("converted from int to Double value:$d")
+    var e ="10"
+    println("String value:$e")
+    var t = e.toInt()
+    println("Converted from string to integer value1:$t")
+    println("Converted from string to integer value2:$t")
+    var w=e.toDouble()
+    println("Converted from string to double value:$w")
+}
