b6166918bbd659e1b0a8f693fbc93bbbc2f017d7
krutikarathod17 <krutikarathod21@gnu.ac.in>
2023-07-18 22:03:44 +0530

    Practical-1.1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var a: Int = 17
+    var b: Double = 12.5678
+    var c: Float = 2.26f
+    var d: Long = 3455627773
+    var e: Short = 1701
+    var f: Byte = 127
+    var g: Char = 'K'
+    var h: Boolean = true
+    var i: String = "Krutika Rathod!"
+    println("Integer value :$a")
+    println("Double value :$b")
+    println("Float value :$c")
+    println("Long value :$d")
+    println("Short value :$e")
+    println("Byte value :$f")
+    println("Char value :$g")
+    println("Boolean value :$h")
+    println("String value :$i")
+}
